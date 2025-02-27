d665a889633c4038528e707ca4cdd2aed159e686
PC <PC@DESKTOP-4TSR235>
2025-02-27 23:33:04 +0300

    Redis added for BasketAPI

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -20,8 +20,22 @@ builder.Services.AddMarten(opts =>
 builder.Services.AddValidatorsFromAssemblies([typeof(Program).Assembly]);
 builder.Services.AddExceptionHandler<CustomExceptionHandler>();
 builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+builder.Services.Decorate<IBasketRepository,CachedBasketRepository>();
+builder.Services.AddStackExchangeRedisCache(opts =>
+{
+    opts.Configuration =builder.Configuration.GetConnectionString("Redis");
+});
+
+builder.Services.AddHealthChecks()
+    .AddNpgSql(builder.Configuration.GetConnectionString("Database")!)
+    .AddRedis(builder.Configuration.GetConnectionString("Redis")!);
 
 var app = builder.Build();
 app.UseExceptionHandler(options =>{ });
 app.MapCarter();
+app.UseHealthChecks("/health",
+    new HealthCheckOptions
+    {
+        ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+    });
 app.Run();
